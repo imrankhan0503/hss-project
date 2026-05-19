382b82e51e6e0f8d716bcedc7a562ccac881b023
Suneetha <sidda24uneetha@gmail.com>
2026-05-19 15:41:25 +0200

    updated with some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../styles/globals.css";
-import Footer from "@/components/Footer/footer";
+import Footer from "@/components/Footer/index";
 
 const inter = Inter({
   variable: "--font-inter",
