f0903320854c374a5d9149285cf53189f75f7dfd
themichellesarmiento <michellechicsarmiento@yahoo.com>
2026-05-26 13:39:42 +0200

    change icon

--- components/HomeComponents/Services/ServiceCard/index.tsx
+++ components/HomeComponents/Services/ServiceCard/index.tsx
@@ -3,14 +3,15 @@ import Image, { StaticImageData } from "next/image";
 type ServiceCardProps = {
   title: string,
   description: string,
-  icon: StaticImageData
+  icon: StaticImageData,
+  resize?: boolean
 }
 
-const ServiceCard = ({ title, description, icon }: ServiceCardProps) => {
+const ServiceCard = ({ title, description, icon ,resize }: ServiceCardProps) => {
   return (
     <div className="flex flex-col items-center text-center gap-4">
       <div className="w-20 h-20 rounded-full bg-primary flex items-center justify-center">
-        <Image src={icon} alt={title}  className="w-14 h-14" />
+        <Image src={icon} alt={title}  className= {`{${resize ? 'w-10 h-10'  : 'w-14 h-14'} object-contain `}/>
         </div>
       <h3 className="text-xl font-bold text-primary">{title}</h3>
       <p className="text-base text-primary max-w-sm text-justify">{description}</p>
