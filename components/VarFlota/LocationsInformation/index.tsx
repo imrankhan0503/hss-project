3a4d335ef6b4e2248fbed2d023ef0979851d90b4
Lesstread666 <vlamaks1604@gmail.com>
2026-05-26 20:32:10 +0200

    change mapUrl to english adaptation

--- components/VarFlota/LocationsInformation/index.tsx
+++ components/VarFlota/LocationsInformation/index.tsx
@@ -10,7 +10,7 @@ const locations = [
         title: "Kårlokal Ruffen",
         address: "Kårlokal Ruffen, vid Hässelby strandbad ",
         description: "Detta är vår huvudsakliga plats vid vattnet, där vi håller våra veckomöten.",
-        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2033.1866839120714!2d17.819428659182392!3d59.36322316830078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9f000adaa135%3A0xf7c26dadb80cbb2f!2sRuffen%20%7C%20H%C3%A4sselby%20Strands%20Sj%C3%B6scoutk%C3%A5r!5e0!3m2!1sru!2sse!4v1779811212675!5m2!1sru!2sse",
+        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2033.1868692705673!2d17.819429476991672!3d59.36322007463075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9f000adaa135%3A0xf7c26dadb80cbb2f!2sRuffen%20%7C%20Hässelby%20Strands%20Sjöscoutkår!5e0!3m2!1sen!2sse!4v1779819942863!5m2!1sen!2sse",
     },
     {
         id: "myset",
@@ -18,7 +18,7 @@ const locations = [
         title: "Hajkstugan Myset",
         address: "Kyrkhamnsvägen, Järfälla (Mellan Kyrkhamn och Gåseborg)",
         description: "Vår mysiga stuga i skogen som används för övernattningar, hajk och landbaserade aktiviteter.",
-        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2031.1610317500754!2d17.7705506!3d59.397025799999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465fa128809e851d%3A0xb0d426cb47615c42!2sScoutstugan%20Myset!5e0!3m2!1sru!2sse!4v1779814836847!5m2!1sru!2sse"
+        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2031.161037187684!2d17.765679692509906!3d59.39702570927691!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465fa128809e851d%3A0xb0d426cb47615c42!2sScoutstugan%20Myset!5e0!3m2!1sen!2sse!4v1779820100522!5m2!1sen!2sse"
     },
     {
         id: "båtklubben",
@@ -26,7 +26,7 @@ const locations = [
         title: "Båtklubben HSBK (Hamn)",
         address: "HSBK ligger längst ner på Sparrisbacken",
         description: "Här har vi våra hamnplatser för kårens större båtar under sommarhalvåret.",
-        mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2033.527243164412!2d17.8317311!3d59.357539!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f7560392fb73d%3A0xa15b8231b471acfb!2sHasselby%20beach%20%26%20boat%20club!5e0!3m2!1sru!2sse!4v1779815023119!5m2!1sru!2sse"
+        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2033.5161070541003!2d17.829436476991244!3d59.357724874628076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f7560392fb73d%3A0xa15b8231b471acfb!2sHasselby%20beach%20%26%20boat%20club!5e0!3m2!1sen!2sse!4v1779820171039!5m2!1sen!2sse"
     },
     {
         id: "varvet",
@@ -34,7 +34,7 @@ const locations = [
         title: "Varvet IRS (Skeppsbyggeri)",
         address: "IRS ligger på Kanaans väg mot Kanaanbadet",
         description: "Detta är vårt båtvarv där vi utför underhåll, reparationer och vinterförvaring.",
-        mapUrl: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2034.1008231540432!2d17.859503!3d59.3479648!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f757057e6558b%3A0xbed2e8f83a8d3203!2zSVJTIOKAkyBJbnRyZXNzZWbDtnJlbmluZ2VuIFLDpWNrc3RhIFNqw7ZoYWdl!5e0!3m2!1sru!2sse!4v1779815069633!5m2!1sru!2sse"
+        mapUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2034.0877052552748!2d17.860526176990614!3d59.34818377462341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f757057e6558b%3A0xbed2e8f83a8d3203!2zSVJTIOKAkyBJbnRyZXNzZWbDtnJlbmluZ2VuIFLDpWNrc3RhIFNqw7ZoYWdl!5e0!3m2!1sen!2sse!4v1779820266061!5m2!1sen!2sse"
     }
 ]
 
@@ -84,7 +84,7 @@ const LocationsInformation = () => {
                             Adress:
                         </p>
 
-                        <p className="mt-2 text-xl">
+                        <p className="mt-2 text-xl ">
                             {activeLocation.address}
                         </p>
 
