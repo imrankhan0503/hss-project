14975675d0947da06389bd22d257b77dc1d126a8
Lesstread666 <vlamaks1604@gmail.com>
2026-06-01 16:03:51 +0200

    Add preview icon to open doc in new tab

--- components/Foraldrar/DocumentsDownloads/index.tsx
+++ components/Foraldrar/DocumentsDownloads/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import DownloadIcon from '@/assets/images/icons/download.svg'
+import PreviewIcon from '@/assets/images/icons/preview.svg'
 
 type DocumentProps = {
     id: number
@@ -7,52 +8,52 @@ type DocumentProps = {
     file: string
 }
 
-const documents:DocumentProps[] = [
+const documents: DocumentProps[] = [
     {
         id: 1,
-        title: "Årsredovisning",
-        file: "/documents/1-arsredovisning.pdf"
+        title: 'Årsredovisning',
+        file: '/documents/1-arsredovisning.pdf',
     },
     {
         id: 2,
-        title: "Revisionsberättelse",
-        file: "/documents/2-revisionsberattelse.pdf"
+        title: 'Revisionsberättelse',
+        file: '/documents/2-revisionsberattelse.pdf',
     },
     {
         id: 3,
-        title: "Verksamhetsplan",
-        file: "/documents/3-verksamhetsplan.pdf"
+        title: 'Verksamhetsplan',
+        file: '/documents/3-verksamhetsplan.pdf',
     },
     {
         id: 4,
-        title: "Budget 2025-2026",
-        file: "/documents/4-budget-2025-2026.pdf"
+        title: 'Budget 2025-2026',
+        file: '/documents/4-budget-2025-2026.pdf',
     },
     {
         id: 5,
-        title: "Sjöscoutkår",
-        file: "/documents/5-sjoscoutkar.pdf"
+        title: 'Sjöscoutkår',
+        file: '/documents/5-sjoscoutkar.pdf',
     },
     {
         id: 6,
-        title: "Anvisningar",
-        file: "/documents/6-anvisningar.pdf"
+        title: 'Anvisningar',
+        file: '/documents/6-anvisningar.pdf',
     },
     {
         id: 7,
-        title: "Kårstämma protokoll",
-        file: "/documents/7-karstamma-protokoll.pdf"
+        title: 'Kårstämma protokoll',
+        file: '/documents/7-karstamma-protokoll.pdf',
     },
     {
         id: 8,
-        title: "Förslag på avgifter",
-        file: "/documents/8-forslag-pa-avgifter.pdf"
+        title: 'Förslag på avgifter',
+        file: '/documents/8-forslag-pa-avgifter.pdf',
     },
     {
         id: 9,
-        title: "Verksamhetsberättelse",
-        file: "/documents/9-verksamhetsberattelse.pdf"
-    }
+        title: 'Verksamhetsberättelse',
+        file: '/documents/9-verksamhetsberattelse.pdf',
+    },
 ]
 
 const DocumentsStorage = () => {
@@ -62,14 +63,30 @@ const DocumentsStorage = () => {
 
             <div>
                 {documents.map((document) => (
-                    <a
-                        key={document.id}
-                        href={document.file}
-                        download
-                    >
+                    <div key={document.id}>
+                        <a
+                            href={document.file}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Image
+                                src={PreviewIcon}
+                                alt="Preview Icon"
+                            />
+                        </a>
+
                         <span>{document.title}</span>
-                        <Image src={DownloadIcon} alt="Download Icon" />
-                    </a>
+
+                        <a
+                            href={document.file}
+                            download
+                        >
+                            <Image
+                                src={DownloadIcon}
+                                alt="Download Icon"
+                            />
+                        </a>
+                    </div>
                 ))}
             </div>
         </section>
