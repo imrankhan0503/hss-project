052a13c88b4b782281aaaf0a976a5c2d84c33391
Imran Khan <emmi_khan2012@yahoo.com>
2026-06-02 18:23:37 +0200

    update hero section content dynamically on language change

--- app/verksamhet/page.tsx
+++ app/verksamhet/page.tsx
@@ -6,13 +6,19 @@ import CcSchedule from "../../components/CcSchedule";
 import { groups } from "../../data/hssdata";
 import HeroSection from "@/components/shared/HeroSection";
 import { heroSectionData } from "@/data/heroSectionData";
+import { t } from "@/i18n/t"
+import { useLang } from "@/i18n/useLang";
 
 const VerksamhetPage = () => {
+  useLang()
+  const data = heroSectionData.verksamhet
   const [expandedId, setExpandedId] = useState<string | null>(null);
 
   return (
     <main className="pb-16">
-      <HeroSection {...heroSectionData.verksamhet} />
+      <HeroSection title={t(data.titleKey)}
+              subtitle={t(data.subtitleKey)}
+               />
 
       <VerksamhetCards
         groups={groups}
