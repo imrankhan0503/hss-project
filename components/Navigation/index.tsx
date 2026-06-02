b39e6cd03089362431e222d1ad938143c28c2e25
themichellesarmiento <michellechicsarmiento@yahoo.com>
2026-06-02 12:49:00 +0200

    merge feature/025-contactus-page after changes

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -51,7 +51,6 @@ const Navigation = () => {
 
 
       <nav className="max-w-7xl mx-auto px-4 py-3 relative overflow-x-hidden">
-
         <div className="flex items-center justify-between gap-6">
 
           <Link
@@ -68,35 +67,21 @@ const Navigation = () => {
               priority
             />
 
-<<<<<<< HEAD
             <div className="text-[#00355F] leading-tight text-sm -mr-4">
 
               <p className="font-semibold text-sm">
-                Hässelby Strands
+
+                {t("headerlogotext.line1")}
               </p>
 
               <p className="font-semibold text-sm">
-                Sjöscoutkår
+
+                {t("headerlogotext.line2")}
               </p>
-=======
-                            <p className="font-semibold text-sm">
-
-                               {t("headerlogotext.line1")}
-                            </p>
-
-                            <p className="font-semibold text-sm">
-                                
-                                {t("headerlogotext.line2")}
-                            </p>
-
-                            <p className="text-xs">
-                                
-                                {t("headerlogotext.line3")}
-                            </p>
->>>>>>> 001-feature-navigation
 
               <p className="text-xs">
-                sedan 1959
+
+                {t("headerlogotext.line3")}
               </p>
 
             </div>
