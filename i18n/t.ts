05d16e20c84530ec45b5224af42f97c8eddb1f1a
Imran Khan <emmi_khan2012@yahoo.com>
2026-06-03 13:00:41 +0200

    cleaned up i18n structure and split into json files

--- i18n/t.ts
+++ i18n/t.ts
@@ -1,11 +1,30 @@
-import sv from "./sv.json"
-import en from "./en.json"
+import enNav from './locales/en/navigation.json'
+import enLogo from './locales/en/logo.json'
+import enHero from './locales/en/herosection.json'
+
+import svNav from './locales/sv/navigation.json'
+import svLogo from './locales/sv/logo.json'
+import svHero from './locales/sv/herosection.json'
+
 import { getLang } from "./langStore"
 
-const dict = { sv, en }
+const dict = {
+  en: {
+    nav: enNav,
+    headerlogotext: enLogo,
+    herosection: enHero
+  },
+  sv: {
+    nav: svNav,
+    headerlogotext: svLogo,
+    herosection: svHero
+  }
+}
 
-export const t = (key: string) => {
-  const lang = getLang()
+export const t = (key: string): string => {
+  if (!key) return "";
+  const lang = getLang();
 
-  return key.split(".").reduce((obj: any, k) => obj?.[k], dict[lang])
+  
+  return (key.split(".").reduce((obj: any, k) => obj?.[k], dict[lang]) as string) || "";
 }
