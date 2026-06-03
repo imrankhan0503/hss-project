a7a32a8d1881fdd6603b13bf07d526985b335e77
themichellesarmiento <michellechicsarmiento@yahoo.com>
2026-06-03 14:26:46 +0200

    fix translation delaying cause

--- components/HomeComponents/ScoutGroups/ScoutGroupCard/index.tsx
+++ components/HomeComponents/ScoutGroups/ScoutGroupCard/index.tsx
@@ -1,5 +1,4 @@
 'use client'
-
 import Image from "next/image";
 import Link from "next/link";
 import { ScoutGroupCardProps } from "@/types/home";
--- components/HomeComponents/Services/ServicesSection/index.tsx
+++ components/HomeComponents/Services/ServicesSection/index.tsx
@@ -1,11 +1,37 @@
 'use client'
 
-import { servicesSectionData } from '@/data/homepageData';
+import SailingIcon from '@/assets/images/homepage/segling.svg'
+import PeopleIcon from '@/assets/images/homepage/gemenskap.svg'
+import GrowthIcon from '@/assets/images/homepage/utveckling.svg'
 import ServiceCard from '../ServiceCard'
+import { ServiceCardData } from '@/types/home';
 import { useLang } from "@/i18n/useLang";
+import { t } from "@/i18n/t";
 
 const ServicesSection = () => {
   useLang()
+
+  const servicesSectionData: ServiceCardData[] = [
+  {
+    id: 1,
+    title: t('homepage.services.sailing.title'),
+    description: t('homepage.services.sailing.description'),
+    icon: SailingIcon,
+    resize: true
+  },
+  {
+    id: 2,
+    title: t('homepage.services.community.title'),
+    description: t('homepage.services.community.description'),
+    icon: PeopleIcon
+  },
+  {
+    id: 3,
+    title: t('homepage.services.development.title'),
+    description: t('homepage.services.development.description'),
+    icon: GrowthIcon
+  },
+]
 
   return (
     <section className="py-16 px-6">
